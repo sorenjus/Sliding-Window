928d289e3950dffb1e3254728b575871492dad30
Meghan Harris <71603666+harrmegh@users.noreply.github.com>
2022-09-24 11:36:55 -0400

    Adding new arrays to house windows and responses

--- client_folder/udpclient.c
+++ client_folder/udpclient.c
@@ -1,14 +1,3 @@
-/*
-char addrIP;
-short portNum = 0;
-
-printf("Enter an IP Address :");
-  //fgets(&addrIP,5000,stdin);
-  scanf("%s", &addrIP);
-  printf("Enter a port number :");
-  scanf("%hd%*c", &portNum);
-printf("%s%hd", &addrIP,portNum);
-*/
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -29,18 +18,23 @@ int main(int argc, char **argv)
 
   char addrIP[20] = "";
   int portNum = 9876;
+  int windowCounter = 0, acknowledgementsSent = 0;
   int receivingWindow[5] = {0, 0, 0, 0, 0};
-  int acknowledgements[5] = {0, 0, 0, 0, 0};
-  // char * to hold the content of the packets
-  char packetInput[255];
+  int acknowledgements[6] = {0, 0, 0, 0, 0, 0};
+  int senderWindow[5], senderReceipt[6];
+  // Holds server response
+  char serverResponse[255];
+  // Holds the file name for writing
+  char *filename;
   // FILE file to write contents to
-  FILE file;
+  FILE *file;
   // Setting up IP, Socket information, and port number
   // printf("Enter an IP Address : \n");
   // fgets(addrIP,5000,stdin);
   // printf("Enter a port number :");
   // scanf("%d%*c", &portNum);
-  struct sockaddr_in serveraddr;
+  struct sockaddr_in serveraddr, clientaddr;
+  socklen_t len = sizeof(clientaddr);
   serveraddr.sin_family = AF_INET;
   serveraddr.sin_port = htons(portNum);
   // serveraddr.sin_addr.s_addr=inet_addr(addrIP);
@@ -48,53 +42,68 @@ int main(int argc, char **argv)
 
   // Asking user for a filename and scanning
   printf("Enter a filename to retrieve: \n");
-  char line[5000];
-  scanf("%s", line);
-  printf("Retrieving %s...\n", line);
+  char userInput[255];
+  scanf("%s", userInput);
+  printf("Retrieving %s...\n", userInput);
+  filename = userInput;
+
+  if ((file = fopen(filename, "w")) == NULL)
+  {
+    fprintf(stderr, "Cannot write to output file");
+    return 1;
+  }
 
   // Send to server
-  sendto(sockfd, &line, strlen(line), 0,
+  sendto(sockfd, &userInput, strlen(userInput), 0,
          (struct sockaddr *)&serveraddr, sizeof(serveraddr));
 
   do
   {
-    // fgets
-    // fputs
-  } while (1);
-
-  /*int32_t firstNum = 5; //guarantees a 32 bit integer
-  int32_t secondNum = 1;
-  int32_t overflowCheck = 0;
-  int result = 0;
-  scanf("%"SCNd32, &firstNum);
-    memcpy(&line[0], &firstNum, 4);
-    printf("Enter a number: ");
-   scanf("%"SCNd32, &secondNum);
-    memcpy(&line[4], &secondNum, 4);
-  sendto(sockfd,line,sizeof(int) * 2,0,
-   (struct sockaddr*)&serveraddr,sizeof(serveraddr));
-*/
-  /*
-    while(1) { //constantly receiving
-        socklen_t len = sizeof(serveraddr);
-        char line[5000] = "\0";
-        bind(sockfd,(struct sockaddr*)&serveraddr,
-             sizeof(serveraddr));
-        recvfrom(sockfd, line, 5000, 0,
-                 (struct sockaddr *) &serveraddr, &len);//client info added to clientaddr
-        memcpy(&result, &line[0], 4);
-
-        memcpy(&overflowCheck, &line[4], 4);
-        if( overflowCheck == 1 ){
-            printf("Got from server: Overflow Error\n");
-        }
-        else {
-            printf("Got from server: %d\n"PRId32, result);
-        }
-        break;
-
+    recvfrom(sockfd, serverResponse, 255, 0,
+             (struct sockaddr *)&serveraddr, &len);
+    printf("Server Response: %s\n", serverResponse);
+    fputs(serverResponse, file);
+    recvfrom(sockfd, senderReceipt, 6, 0,
+             (struct sockaddr *)&serveraddr, &len);
+    printf("Received packet\n");
+    // Now we have the sender receipt array
+    // If the sender receipt matches the order we were expecting
+    if (windowCounter == senderReceipt[5] && senderReceipt[windowCounter] == 1)
+    {
+      printf("Window Counter: %d\n", windowCounter);
+      printf("Sender Receipt: %d\n", senderReceipt[5]);
+      printf("Sender receipt at counter: %d\n", senderReceipt[windowCounter]);
+      receivingWindow[windowCounter] = 1;
+      acknowledgements[windowCounter] = 1;
+      // This array is of length 6.  0-4 for the acknowledgements and the 5th
+      // spot for the index that was just acknowledged.
+      acknowledgements[5] = windowCounter;
+      sendto(sockfd, acknowledgements, 6, 0,
+             (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+      acknowledgementsSent++;
+      windowCounter++;
     }
-*/
+    // what happens when the sender sends a packet in a different order
+    else
+    {
+      printf("Window Counter: %d\n", windowCounter);
+      printf("Sender Receipt: %d\n", senderReceipt[5]);
+      printf("Sender receipt at counter: %d\n", senderReceipt[windowCounter]);
+
+      windowCounter = senderReceipt[5];
+      receivingWindow[windowCounter] = 1;
+      acknowledgements[windowCounter] = 1;
+      acknowledgements[5] = windowCounter;
+      sendto(sockfd, acknowledgements, 6, 0,
+             (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+      acknowledgementsSent++;
+      windowCounter++;
+    }
+    // windowCounter++;
+
+  } while (windowCounter < 5);
+
+  fclose(file);
   close(sockfd);
 
   return 0;
