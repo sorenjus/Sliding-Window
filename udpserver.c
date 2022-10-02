9615b7e12d4c15208f0b0fd9174ca8e961205423
sorenjus <91398241+sorenjus@users.noreply.github.com>
2022-10-02 07:54:21 -0400

    documentation updated

--- udpserver.c
+++ udpserver.c
@@ -11,43 +11,53 @@
 
 int main(int argc, char **argv)
 {
+	//define the socket to be used by the server
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     int portNum = 9876;
-    // printf("Enter a port number :");
-    // scanf("%d%*c", &portNum);
-    struct timeval timeout;
-    timeout.tv_sec = 5;
-    timeout.tv_usec = 0;
-    
     struct sockaddr_in serveraddr, clientaddr; // family-type of address|port-deliver to correct app|s_addr-IP address
-    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(portNum);    // htons - creates a two byte number idn the right order
     serveraddr.sin_addr.s_addr = INADDR_ANY; // sender to any IP address (specified by client)
-
     bind(sockfd, (struct sockaddr *)&serveraddr,
          sizeof(serveraddr)); // ties the socket to the address
+	socklen_t len = sizeof(clientaddr);
+	
+	//define the timeout interval to be use to detect a timeout
+	struct timeval timeout;
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+	setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+	
+	//array holds the current fileSequence for each window (default 0)
     int senderWindow[5] = {0, 0, 0, 0, 0};
-    // int senderReceipt[6] = {0, 0, 0, 0, 0, 0};
-    // int clientAcknowledgements[6];
-    int currentWindowCounter = 0;
-    int currentCount = 0;
+	
+	//when an acknowledgement is received, store the acknowledgements windowCounter and file sequence
+    int receivedWindowCounter = 0;
+    int receivedSeqCount = 0;
     char ackLine[8];
+	
+//the current data to be send in the packet
     char windowValue[5][255];
     // Tracks the current window in the queue
     int windowCounter = 0;
-    // Track the current sequence count
+    // Track the current sequence count of the file
     int fileSequence = 0;
     // fileName set to the udpclient entry
-    socklen_t len = sizeof(clientaddr);
     char fileRequested[5000] = "";
+	while(1){
+		//received packet from the client with the file name
     recvfrom(sockfd, fileRequested, 5000, 0,
              (struct sockaddr *)&clientaddr, &len); // client info added to clientaddr
     char *fileName = fileRequested;
     FILE *file;
+//Read the requested file
     file = fopen(fileName, "r+");
+		//if the file is Null return the error message
     if (file == NULL)
     {
+	    /*this should be send to the client as well, 
+	    they dont know there was an error
+	    */
         printf("Error! Could not open file\n");
         exit(-1);
     }
@@ -56,9 +66,6 @@ int main(int argc, char **argv)
     // Wait for the client to make a request
     if (strcmp(fileRequested, "") != 0)
     {
-        // Array to  track if the desired window is available
-        // printf("%s\n", &windowValue[windowCounter]);
-
         do
         {
             if (senderWindow[windowCounter] == 0)
@@ -112,20 +119,21 @@ Move the recurve from server here, then resend anything from window counter and 
             	}
             } else {
 
-                memcpy(&currentWindowCounter, &ackLine[0], 4);
-                memcpy(&currentCount, &ackLine[4], 4);
+                memcpy(&receivedWindowCount, &ackLine[0], 4);
+                memcpy(&receivedSeqCount, &ackLine[4], 4);
 /*change this to reset when the server sequence = ack sequence
 */
-                senderWindow[currentWindowCounter] = 0;
+                senderWindow[receivedWindowCounter] = 0;
                 char *thing;
                 thing = "";
-                strcpy(&windowValue[currentWindowCounter][0], thing);
-                printf("sender window boolean : %d\nReturn window value : %d\nReturned sequence number : %d\n\n", senderWindow[currentWindowCounter], currentWindowCounter, currentCount);
+                strcpy(&windowValue[receivedWindowCounter][0], thing);
+                printf("sender window boolean : %d\nReturn window value : %d\nReturned sequence number : %d\n\n", senderWindow[receviedWindowCounter], receivedWindowCounter, receivedSeqCount);
             }
             
         } while (!feof(file));
 	//while ()
-	// checking our file sequence to make them all zero
+	    
+	//iterate through the sending window to ensure all packets have been acknowledged
     do{
     for(int i = 0; i < 5; i++){
         if(senderWindow[i] != 0){
@@ -159,6 +167,9 @@ Move the recurve from server here, then resend anything from window counter and 
     } while(senderWindow[0] != 0 && senderWindow[1] != 0 && senderWindow[2] != 0 &&
         senderWindow[3] != 0 && senderWindow[4] != 0);
 
+	    /*remove this?
+	    do a time out to handle the EOF
+	    */
     do{
         printf("Got here\n");
         char *str = "EOF";
@@ -176,6 +187,7 @@ Move the recurve from server here, then resend anything from window counter and 
 	// another while loop after we exit
 	// keep running until we receive back from the client "EOF"
         fclose(file);
+    }
 
         close(sockfd);
     }
